f37e4a255836d153fd49872b9206dd030053adbb
Sojib Das <sojibdas1015@gmail.com>
2026-04-15 21:03:05 +0600

    login with google continue

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../Component/Firebase/firebase.init';
 
 const AuthProvider = ({ children }) => {
     const [user, setuser] = useState(null)
     const [loading, setLoading] = useState(true)
+    const googleProvider = new GoogleAuthProvider()
     const resiterUser = (email, password) => {
         setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
@@ -17,6 +18,9 @@ const AuthProvider = ({ children }) => {
     const signOutUser = () => {
         setLoading(true)
         return signOut(auth)
+    }
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider)
     }
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -32,6 +36,7 @@ const AuthProvider = ({ children }) => {
         resiterUser,
         loginUser,
         signOutUser,
+        signInWithGoogle,
         loading
     }
     return (
